4f090df27aaca3130fd94992fc6714eb842d2a9c
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2024-01-31 19:45:58 -0800

    Macros are cool
    Big macro to generate the code

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,12 +1,10 @@
 use fastrand::Rng;
 use std::{
-    fs::File,
-    io::{Error, Read},
-    path::PathBuf,
+    fs::File, io::{Error, Read}, ops::Index, path::PathBuf
 };
 use winit::event::{ElementState, Event, VirtualKeyCode as VKC, WindowEvent};
+use crate::{font::FONT_SET};
 
-use crate::font::FONT_SET;
 
 pub const WIDTH: usize = 64;
 pub const HEIGHT: usize = 32;
@@ -15,30 +13,17 @@ const PIXEL_ON: &[u8] = &[255, 255, 255, 255];
 const PIXEL_OFF: &[u8] = &[0, 0, 0, 255];
 
 const CONTROLS: [VKC; 16] = [
-    VKC::Key1,
-    VKC::Key2,
-    VKC::Key3,
-    VKC::Key4,
-    VKC::Q,
-    VKC::W,
-    VKC::E,
-    VKC::R,
-    VKC::A,
-    VKC::S,
-    VKC::D,
-    VKC::F,
-    VKC::Z,
-    VKC::X,
-    VKC::C,
-    VKC::V,
+    VKC::Key1,  VKC::Key2,  VKC::Key3,  VKC::Key4,
+    VKC::Q,     VKC::W,     VKC::E,     VKC::R,
+    VKC::A,     VKC::S,     VKC::D,     VKC::F,
+    VKC::Z,     VKC::X,     VKC::C,     VKC::V,
 ];
-
 pub struct Chip8Interpreter {
     memory: [u8; 4096],
     registers: [u8; 16],
     address: u16,
     program_counter: u16,
-    stack: [u16; 16],
+    stack: Vec<u16>,
     stack_ptr: usize,
     rng: Rng,
     vram: [u8; WIDTH * HEIGHT],
@@ -46,6 +31,7 @@ pub struct Chip8Interpreter {
     sound_timer: u8,
     should_execute: bool,
     keyboard: [bool; 16],
+    pub total_dt: f32,
     beep: bool,
 }
 impl Chip8Interpreter {
@@ -58,7 +44,7 @@ impl Chip8Interpreter {
             registers: [0; 16],
             address: 0,
             program_counter: 0x200,
-            stack: [0; 16],
+            stack: vec![],
             delay_timer: 0,
             sound_timer: 0,
             vram: [0; WIDTH * HEIGHT],
@@ -66,6 +52,7 @@ impl Chip8Interpreter {
             should_execute: false,
             keyboard: [true; 16],
             beep: false,
+            total_dt: 0.0,
         }
     }
     // Gets keyboard events from winit's eventloop, only processing the keys listed in `CONTROLS`
@@ -116,192 +103,215 @@ impl Chip8Interpreter {
             }
         }
     }
-    pub fn error(&self, opcode: u16) {
-        panic!("Unsupported opcode: {opcode:#x}");
+    fn update_timer(&mut self, dt: f32) {
+        if self.delay_timer > 0 {
+            self.total_dt += dt;
+            const TIMER_PERIOD: f32 = 1.0 / 60.0;
+            while self.total_dt > TIMER_PERIOD {
+                self.total_dt -= TIMER_PERIOD;
+                self.delay_timer = self.delay_timer.wrapping_sub(1);
+            }
+        }
     }
+
     pub fn execute_cycle(&mut self, pixels: &mut [u8]) {
         if !self.should_execute {
             return;
         }
-        self.delay_timer = self.delay_timer.wrapping_sub(1);
-        // Fetch
+        // TODO
+        self.update_timer(1.0/60.0);
+
         let opcode = {
             let location = self.program_counter as usize;
             let mem = [self.memory[location], self.memory[location + 1]];
             u16::from_be_bytes(mem)
         };
+        self.program_counter += 2;
+        
+        self.handle_opcode(opcode, pixels);
 
-        // Decode
-        let address = opcode & 0x07FF;
+    }
+}
+
+macro_rules! opcode_equals {
+    ($opcode:expr, $pattern:expr) => {
+        {
+            // Convert pattern to hex with wildcards zeroed
+            let match_operation = u16::from_str_radix(&$pattern
+                .replace("x", "0")
+                .replace("y", "0")
+                .replace("k", "0")
+                .replace("n", "0"), 16)
+                .unwrap();
+            // Create a bitmask for all non wildcards
+            let mut match_mask = 0;
+            for (i,c) in $pattern.chars().rev().enumerate() {
+                if !matches!(c, 'x' | 'y' | 'k' | 'n') {
+                    match_mask += 0b1111 << i * 4
+                }
+            }
+            // Compares opcodes when wildcards have been zeroed
+            $opcode & match_mask == match_operation
+        }
+    };
+}
+macro_rules! opcode_handler {
+    ($opcode:expr, $($pattern:literal => $code:block),+) => {
+        match true {
+            $(
+                true if opcode_equals!($opcode, $pattern) => $code
+            )*
+            _ => {panic!("Unknown opcode: {:#4x}",$opcode)}
+        }
+    };
+
+}
+impl Chip8Interpreter {
+    fn handle_opcode(&mut self, opcode: u16, pixels: &mut [u8]) {
         let x = ((opcode & 0x0F00) >> 8) as usize;
         let y = ((opcode & 0x00F0) >> 4) as usize;
         let byte = opcode & 0x00FF;
+        let address = opcode & 0x07FF;
         let nimble = opcode & 0x000F;
 
-        // Execute
-        match opcode >> 12 {
-            0x0 => {
-                match nimble {
-                    // Clear the display.
-                    0x0 => self.clear_display(pixels),
-                    // Return from a subroutine.
-                    0xE => {
-                        self.program_counter = self.stack[self.stack_ptr];
-                        self.stack_ptr -= 1;
-                    }
-                    _ => self.error(opcode),
+        opcode_handler!(opcode,
+            "00e0" => {
+                self.clear_display(pixels)
+            },
+            "00ee" => {
+                if let Some(adr) = self.stack.pop() {
+                    self.program_counter = adr
                 }
-            }
-            // Jump to location
-            0x1 => {
-                self.program_counter = address - 2;
-            }
-            // Call subroutine
-            0x2 => {
-                self.stack_ptr += 1;
-                self.stack[self.stack_ptr] = self.program_counter;
-            }
-            // Skip instruction on condition
-            0x3 => {
+            },
+            "1nnn" => {
+                self.program_counter = address;
+            },
+            "2nnn" => {
+                self.stack.push(self.program_counter);
+                self.program_counter = address;
+            },
+            "3xkk" => {
                 if self.registers[x] as u16 == byte {
                     self.program_counter += 2;
                 }
-            }
-            // Skip instruction on condition if not equal
-            0x4 => {
+            },
+            "4xkk" => {
                 if self.registers[x] as u16 != byte {
                     self.program_counter += 2;
                 }
-            }
-            // Skip instruction on condition if equal
-            0x5 => {
+            },
+            "5xy0" => {
                 if self.registers[x] == self.registers[y] {
                     self.program_counter += 2;
                 }
-            }
-            // Set register
-            0x6 => {
+            },
+            "6xkk" => {
                 self.registers[x] = byte as u8;
-            }
-            // Increment register
-            0x7 => {
+            },
+            "7xkk" => {
                 self.registers[x] += byte as u8;
-            }
-            // Math operations
-            0x8 => {
-                match nimble {
-                    // Copies register
-                    0x0 => {
-                        self.registers[x] = self.registers[y];
-                    }
-                    // Bitwise or
-                    0x1 => {
-                        self.registers[x] |= self.registers[y];
-                    }
-                    // Bitwise and
-                    0x2 => {
-                        self.registers[x] &= self.registers[y];
-                    }
-                    // Bitwise xor
-                    0x3 => {
-                        self.registers[x] ^= self.registers[y];
-                    }
-                    // Add overflow
-                    0x4 => {
-                        let intermediate = self.registers[x] as u16 + self.registers[y] as u16;
-                        self.registers[0xf] = (intermediate > 0xff) as u8;
-                        self.registers[x] = (intermediate & 0xff) as u8;
-                    }
-                    // Subtract
-                    0x5 => {
-                        self.registers[0xf] = (self.registers[x] > self.registers[y]) as u8;
-                        self.registers[x] = self.registers[x].wrapping_sub(self.registers[y]);
-                    }
-                    // Divide
-                    0x6 => {
-                        self.registers[0xf] = self.registers[x] & 0x1;
-                        self.registers[x] /= 2;
-                    }
-                    // Subtract
-                    0x7 => {
-                        self.registers[0xf] = (self.registers[y] > self.registers[x]) as u8;
-                        self.registers[x] = self.registers[y].wrapping_sub(self.registers[x]);
-                    }
-                    // Multiply
-                    0xe => {
-                        self.registers[0xf] = self.registers[x] >> 7;
-                        self.registers[x] *= 2;
-                    }
-                    _ => self.error(opcode),
-                }
-            }
-            // Skip instruction if not equal
-            0x9 => {
+            },
+            "8xy0" => {
+                self.registers[x] = self.registers[y];
+            },
+            "8xy1" => {
+                self.registers[x] |= self.registers[y];
+            },
+            "8xy2" => {
+                self.registers[x] &= self.registers[y];
+            },
+            "8xy3" => {
+                self.registers[x] ^= self.registers[y];
+            },
+            "8xy4" => {
+                let intermediate = self.registers[x] as u16 + self.registers[y] as u16;
+                self.registers[0xf] = (intermediate > 0xff) as u8;
+                self.registers[x] = (intermediate & 0xff) as u8;
+            },
+            "8xy5" => {
+                self.registers[0xf] = (self.registers[x] > self.registers[y]) as u8;
+                self.registers[x] = self.registers[x].wrapping_sub(self.registers[y]);
+            },
+            "8xy6" => {
+                self.registers[0xf] = self.registers[x] & 0x1;
+                self.registers[x] /= 2;
+            },
+            "8xy7" => {
+                self.registers[0xf] = (self.registers[y] > self.registers[x]) as u8;
+                self.registers[x] = self.registers[y].wrapping_sub(self.registers[x]);
+            },
+            "8xyE" => {
+                self.registers[0xf] = self.registers[x] >> 7;
+                self.registers[x] *= 2;
+            },
+            "9xy0" => {
                 if self.registers[x] != self.registers[y] {
                     self.program_counter += 2;
                 }
-            }
-            // Set address
-            0xa => {
-                self.address = address;
-            }
-            // Jump to address
-            0xb => {
-                self.program_counter = address + self.registers[0x0] as u16 - 2;
-            }
-            // Random byte
-            0xc => {
-                self.registers[x] = self.rng.u8(0..255) & byte as u8;
-            }
-            // Draw sprite
-            0xd => {
-                self.draw_sprite(x, y, nimble, pixels);
-            }
-            // Keyboard input
-            0xe => match byte {
-                0x9e => {
-                    if (self.keyboard[self.registers[x] as usize]) {
-                        self.program_counter += 2;
-                    }
-                }
-                0xa1 => {
-                    if (!self.keyboard[self.registers[x] as usize]) {
-                        self.program_counter += 2;
-                    }
-                }
-                _ => self.error(opcode),
             },
-            0xf => {
-                match byte {
-                    // TODO: Sound
-                    0xf7 => {}
-                    0x0a => {}
-                    0x15 => {}
-                    0x18 => {}
-                    // Increment address
-                    0x1e => {
-                        self.address += self.registers[x] as u16;
-                    }
-                    // Load sprite address
-                    0x29 => self.address = self.registers[x] as u16 * 5,
-                    // Copy registers into memory
-                    0x55 => {
-                        let mem = &mut self.memory[address as usize..address as usize + x as usize];
-                        mem.copy_from_slice(&self.registers[0..x as usize]);
-                    }
-                    // Copy registers from  memory
-                    0x65 => {
-                        let mem = &mut self.memory[address as usize..address as usize + x as usize];
-                        self.registers[0..x as usize].copy_from_slice(&mem);
-                    }
-
-                    _ => self.error(opcode),
+            "Annn" => {
+                self.address = address;
+            },
+            "Bnnn" => {
+                self.program_counter = address + self.registers[0x0] as u16;
+            },
+            "Cxkk" => {
+                self.registers[x] = self.rng.u8(0..255) & byte as u8;
+            },
+            "Dxyn" => {
+                self.draw_sprite(x, y, nimble, pixels);
+            },
+            "Ex9E" => {
+                if self.keyboard[self.registers[x] as usize] {
+                    self.program_counter += 2;
                 }
+            },
+            "ExA1" => {
+                if !self.keyboard[self.registers[x] as usize] {
+                    self.program_counter += 2;
+                }
+            },
+            // No audio yet
+            "Fx07" => {
+                self.registers[x] = self.delay_timer;
+            },
+            "Fx0A" => {
+                // TODO: use keydown event and make nicer
+                if (self.keyboard.iter().any(|x| *x)) {
+                    self.registers[x as usize] = self.keyboard.iter().position(|x| *x).unwrap() as u8;
+                } else {
+                    self.program_counter -= 2;
+                }
+            },
+            "Fx15" => {
+                self.delay_timer = self.registers[x] + 1;
+            },
+            "Fx18" => {
+                self.sound_timer = self.registers[x];
+            },
+            "Fx1E" => {
+                self.address += self.registers[x] as u16;
+            },
+            "Fx29" => {
+                self.address = self.registers[x] as u16 * 5
+            },
+            "Fx33" => {
+                self.memory[self.address as usize] = self.registers[x] / 100;
+                self.memory[self.address as usize + 1] = (self.registers[x] / 10) % 10;
+                self.memory[self.address as usize + 2] = self.registers[x] % 10;
+            },
+            "Fx55" => {
+                let mem = &mut self.memory[address as usize..address as usize + x as usize];
+                mem.copy_from_slice(&self.registers[0..x as usize]);
+                self.address += (x + 1) as u16;
+            },
+            "Fx65" => {
+                let mem = &mut self.memory[address as usize..address as usize + x as usize];
+                self.registers[0..x as usize].copy_from_slice(&mem);
+                self.address += (x + 1) as u16;
             }
 
-            _ => self.error(opcode),
-        }
-        // Go to next instruction
-        self.program_counter += 2;
+        );
+        // println!("{opcode:x} {:?}", (self.registers, self.address, self.program_counter, &self.stack, self.vram))
     }
 }
